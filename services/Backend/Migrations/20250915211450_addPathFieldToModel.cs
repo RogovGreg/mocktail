f6373dbf107af56a1878c1bb0099893d478ea0c1
Svetozar Ikovic <svetozar.ikovic@adaandthemachine.com>
2025-09-15 23:19:33 +0200

    Create migrations for path

---
+++ services/Backend/Migrations/20250915211450_addPathFieldToModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPathFieldToModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Endpoints",
+                table: "Projects");
+
+            migrationBuilder.RenameColumn(
+                name: "KeyWords",
+                table: "Templates",
+                newName: "Tags");
+
+            migrationBuilder.RenameColumn(
+                name: "templates",
+                table: "Projects",
+                newName: "Templates");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "Templates",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "KeyWords",
+                table: "Projects",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "KeyWords",
+                table: "Projects");
+
+            migrationBuilder.RenameColumn(
+                name: "Tags",
+                table: "Templates",
+                newName: "KeyWords");
+
+            migrationBuilder.RenameColumn(
+                name: "Templates",
+                table: "Projects",
+                newName: "templates");
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "Endpoints",
+                table: "Projects",
+                type: "uuid[]",
+                nullable: true);
+        }
+    }
+}
